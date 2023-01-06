6ddc2311874ab69d77cbf47cacefa8a25334dead
Bochia <brandon.ochia@gmail.com>
2023-01-06 14:20:17 +0100

    Added TODO comment.

--- PDFTools/Services/SplitRangeValidator.cs
+++ PDFTools/Services/SplitRangeValidator.cs
@@ -9,6 +9,12 @@
         public ServiceResponse ValidateSplitRanges(string ranges)
         {
             throw new NotImplementedException();
+            //TODO:
+            //maybe before string passed you should remove all whitespaces from the ranges.
+            //Needs to validate that string is not null, empty, or whitespace.
+            //Needs to validate string only has expected characters.
+            //validate that each range starts with a number smaller than the end number in the range. 
+            //validate that a range has larger numbers than the last range. 
         }
     }
 }
