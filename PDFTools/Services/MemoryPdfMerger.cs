e4521e9d8fa3c2a73d97976df5d89710c2774220
Brandon Ochia <brandon.ochia@gmail.com>
2024-08-25 19:14:45 +0200

    Added Dispose() logic to MemoryPdfMerger.
    If something fails need to dispose of PdfDocuments and Streams so we dont get a memory leak.

--- PDFTools/Services/MemoryPdfMerger.cs
+++ PDFTools/Services/MemoryPdfMerger.cs
@@ -34,6 +34,8 @@
                 }
             }
 
+            MemoryStream outputPdfStream = new MemoryStream();
+
             try
             {
                 using (PdfDocument outputPdf = new PdfDocument())
@@ -60,7 +62,6 @@
                         }
                     }
 
-                    MemoryStream outputPdfStream = new MemoryStream();
                     outputPdf.Save(outputPdfStream, false); // Leave the stream open - it's up to the caller to close it.
 
                     return new Attempt<Stream>()
@@ -72,6 +73,7 @@
             }
             catch (Exception ex)
             {
+                outputPdfStream.Dispose();
                 return new Attempt<Stream>()
                 {
                     ErrorMessage = $"Failed to merge PDFs - {ex.Message}"
